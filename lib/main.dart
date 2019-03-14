4c8ceebe28522e882f1c4933fee54af2ec761c15
Rody Davis <rdavis@unifycrm.com>
2019-03-14 10:16:12 -0400

    Adding folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gmail_clone/ui/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(),
+      home: HomeScreen(),
     );
   }
 }
