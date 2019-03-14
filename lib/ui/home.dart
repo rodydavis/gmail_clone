62b4435d5ebe4743949b3f8b29401c7654335376
Rody Davis <rdavis@unifycrm.com>
2019-03-14 10:48:43 -0400

    Adding Gradient

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -59,12 +59,30 @@ class HomeScreen extends StatelessWidget {
       ],
       itemCount: 100,
       itemBuilder: (BuildContext context, int index) {
-        return  EmailListTile(
+        return EmailListTile(
           index: index,
         );
       },
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+        backgroundColor: Theme.of(context).brightness == Brightness.dark
+            ? null
+            : Colors.white,
+        child: ShaderMask(
+          shaderCallback: (Rect bounds) {
+            return RadialGradient(
+              center: Alignment.center,
+              radius: 1.0,
+              colors: <Color>[
+                Colors.yellowAccent,
+                Colors.blueAccent,
+                Colors.redAccent,
+                Colors.greenAccent,
+              ],
+              tileMode: TileMode.mirror,
+            ).createShader(bounds);
+          },
+          child: Icon(Icons.add, size: 40.0),
+        ),
         onPressed: () {},
       ),
     );
