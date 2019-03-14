0f9ba62db8220c2a97a8423f312f610cc4e8832c
Rody Davis <rdavis@unifycrm.com>
2019-03-14 10:14:36 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Gmail Clone',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
